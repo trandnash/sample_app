365239ef1ae96e681c1a324a0eedb29eebae6d0f
Thomas Rand-Nash <t.randnash@gmail.com>
2013-04-23 15:33:45 -0700

    Finish sign in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
   def create
   	@user = User.new(params[:user]) #this is equivalent to grabbing all user attributes, :user is a hash of hashes
     if @user.save
+      sign_in @user
     	flash[:success] = "Welcome to The Fuelist!"
   		redirect_to @user
   	else
